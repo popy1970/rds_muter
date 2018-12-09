fdb1e0f4a7d8f24573ba2b637759bc053a7b0456
Daniel Provencher <popy01970@gmail.com>
2018-12-09 18:23:00 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+Chrome extension Rds muter
+================================
+
+Extension permetant de muter sur rds.ca
+
+Procédure d'installation
+
+Aller dans les paramètres de CHROME, et cliquer sur les 3 barres à gauche.
+
+ensuite dans extensions, activer le Mode développeur
+
+cliquer sur charger l'extension non empaquetées, ensuite choisir le répertoire ou vous avez décompresser l'extension.
+
---
+++ src/background.js
@@ -0,0 +1,58 @@
+
+setTimeout(addFirstbutton, 10000)
+var mutedUser=[];
+function addFirstbutton()
+{
+	
+	
+	$(".gig-comment-username").each(function(){
+		username = $(this).html();
+		$(this).after('<button class="muteme" value="'+username+'"> Mute </button>');
+	});
+	$(".muteme").click(function(){
+		mutedUser.push($(this).attr("value"));
+		
+		console.log(mutedUser);
+		$(".gig-comment-username").each(function(){
+			username = $(this).html();
+			if ($.inArray( username, mutedUser ) != -1)
+			{
+				$(this).closest(".gig-comment-self-data").hide();
+				$(this).closest(".gig-comment").find(".gig-comment-photo").hide();
+			}
+		});
+	});
+//	var bloclist = $(".gig-comment-username");
+//	var username;
+//	for(var i = 0; i < bloclist.length; i++){
+//		username = bloclist[i].innerHTML;
+//	    $(this).after('<button value="'+username+'">test</button>');
+//	}
+//	$(".gig-comment-username").after('<button value="blabla">test</button>');
+}
+
+function addnewbutton()
+{
+	$(".gig-comment-newComment").find(".gig-comment-username").each(function(){
+		username = $(this).html();
+		if ($.inArray( username, mutedUser ) != -1)
+		{
+			$(this).closest(".gig-comment-self-data").hide();
+			$(this).closest(".gig-comment").find(".gig-comment-photo").hide();
+		}
+		if ($(this).parent().find(".muteme").length == 0){
+				$(this).after('<button class="muteme" value="'+username+'"> Mute </button>');
+				$(this).parent().find(".muteme").click(function(){
+					mutedUser.push($(this).attr("value"));
+					username = $(this).attr("value");
+					if ($.inArray( username, mutedUser ) != -1)
+					{
+						$(this).closest(".gig-comment-self-data").hide();
+						$(this).closest(".gig-comment").find(".gig-comment-photo").hide();
+					}
+					});
+		}
+		
+	});
+}
+setInterval(addnewbutton, 1000);
