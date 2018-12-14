8c798b5de130b9e0bbce5c96ef53e6120e698f59
Daniel.Provencher <Daniel.Provencher@192.168.2.119>
2018-12-14 15:13:23 -0500

    Quelques bugs fix, et ajout de la liste des usagers

--- src/background.js
+++ src/background.js
@@ -1,13 +1,25 @@
 
 setTimeout(addFirstbutton, 10000)
 var mutedUser=[];
+var userlist =[];
 function addFirstbutton()
 {
 	
-	
+
+	if ($("#commentsDiv").length > 0)
+	{
+		$(".form-popup").show();
+	}
+
 	$(".gig-comment-username").each(function(){
 		username = $(this).html();
 		$(this).after('<button class="muteme" value="'+username+'"> Mute </button>');
+		if ($.inArray( username, userlist ) == -1)
+			{
+				
+				userlist.push(username);
+				$("#user_container").append('<div class="usernameListe"><button class="muteme" value="'+username+'"> Mute </button>&nbsp;&nbsp;&nbsp;'+username+'</div>');
+			}
 	});
 	$(".muteme").click(function(){
 		mutedUser.push($(this).attr("value"));
@@ -63,4 +75,20 @@ function addnewbutton()
 		
 	});
 }
+var box = '<div class="form-popup"><div id="popup_header">Liste des usagers</div>\
+	<div id="user_container" class="hidden"></div>\
+	</div>';
+$("body").prepend(box);
+
+$("#popup_header").click(function(){
+	if ($("#user_container").hasClass("hidden"))
+		{
+			$("#user_container").removeClass("hidden")
+		}
+	else
+		{
+			$("#user_container").addClass("hidden")
+		}
+});
 setInterval(addnewbutton, 1000);
+
